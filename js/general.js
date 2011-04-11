77d28a3369825901551e214a1b747b5d247088b8
Alex Kamotsky <kamotsky@gmail.com>
2011-04-11 00:32:14 -0700

    Added timer, reworked UI, removed comments from JS.

--- js/general.js
+++ js/general.js
@@ -1,25 +1,31 @@
- jQuery(document).ready(function(){
-   //var cookieName = 'classesList';
-   //$("#verbose").val("");
-   //var listHash = readMyListHash(cookieName);
-   //updateMyListCounter( listHash );
+jQuery(document).ready(function(){
 
-   //localStorage["testKey"] = "testValue";
+   g_isTimerOn = false;
 
    $("button.add").click(function(e){
-      var pom = makePom();
-      pom.insertBefore($(this));
+      if( g_isTimerOn ) return;
+      
+      var thisTask = $(this).closest(".task");
+      //grayOutAllTasksButThis( thisTask ); // should also disable the button.
+      
+      g_taskTimeElement = thisTask.find(".timer").last();
+      g_secondsLeft = 5;//25*60;
+      g_isTimerOn = true;
+      g_intervalId = setInterval("countDown()", 1000);
+      
+      //var pom = makePom();
+      //pom.insertBefore($(this));
    });
 
    $("button.del").click(function(e){
-      $(this).parent("task").children("img").last().remove();
+      $(this).closest(".task").find("img").last().remove();
    });
    
    $("input").keyup(function(e){
       if(e.keyCode == 13){
-         var thisTask = $(this).parent("task");
+         var thisTask = $(this).closest(".task");
          var newTask  = thisTask.clone( true, true );
-         var newInput = newTask.children("input").attr("val", "");
+         var newInput = newTask.find("input").attr("val", "");
          newTask.children("img").remove();
          var pom = makePom();
          pom.insertAfter(newInput);
@@ -27,34 +33,35 @@
          newInput.focus();
       }
    });
-
-   //$(".addToMyList input").click(function(e){
-      //$(this).closest("li").addClass("selected");
-
-      //var thisClassID = $(this).closest(".item").attr("id");
-
-      //var listHash = readMyListHash(cookieName);
-      //listHash[ thisClassID ] = 1;
-      //saveMyListHash( listHash, cookieName );
-      //updateMyListCounter( listHash );
-
-      //e.preventDefault();
-   //});
-
-   //$(".removeFromMyList input").click(function(e){
-      //$(this).closest("li").removeClass("selected");
-
-      //var thisClassID = $(this).closest(".item").attr("id");
-
-      //var listHash = readMyListHash(cookieName);
-      //delete listHash[ thisClassID ];
-      //saveMyListHash( listHash, cookieName );
-      //updateMyListCounter( listHash );
-
-      //e.preventDefault();
-   //});
 });
 
 function makePom() {
    return $('<img src="css/pom.jpg" />');
+}
+
+function grayOutAllTasksButThis( task ) {
+   //var article = task.parent("article");
+   //article.children(".task").addClass("gray");
+   //task.removeClass("gray");
+}
+
+function countDown() {
+   if( --g_secondsLeft > 0 ) {
+      var min = Math.floor( g_secondsLeft / 60 );
+      var sec = g_secondsLeft % 60;
+      var minStr = toTwoDigitString( min );
+      var secStr = toTwoDigitString( sec );
+      g_taskTimeElement.text( minStr + ':' + secStr );
+   }
+   else {
+      g_taskTimeElement.text( '00:00' );
+      clearInterval( g_intervalId );
+      g_isTimerOn = false;
+   }
+}
+
+function toTwoDigitString( v ) {
+   var vStr = v.toString();
+   if( vStr.length < 2 ) return '0' + vStr;
+   return vStr;
 }
