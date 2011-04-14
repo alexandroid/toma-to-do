ba4f285304d391eb11486668deba77921bdd3284
Alex Kamotsky <kamotsky@gmail.com>
2011-04-13 22:53:42 -0700

    Reworked UI, added rest button. Almost done.

--- js/general.js
+++ js/general.js
@@ -1,20 +1,38 @@
 jQuery(document).ready(function(){
 
+   g_cookieName = "tasksAndPomodoros";
    g_isTimerOn = false;
+   g_storageErrorHappened = false;
+   g_timeoutId = null;
 
-   $("button.add").click(function(e){
+   $("button.work").click(function(e){
       if( g_isTimerOn ) return;
       
       var thisTask = $(this).closest(".task");
-      //grayOutAllTasksButThis( thisTask ); // should also disable the button.
+      thisTask.addClass("activetask").removeClass("task");
+      thisTask.parent("article").find(".task").each( function(index) {
+         if ($(this) != thisTask) {
+            $(this).addClass("inactivetask").removeClass("task");
+         }
+      });
       
       g_taskTimeElement = thisTask.find(".timer").last();
       g_secondsLeft = 5;//25*60;
       g_isTimerOn = true;
-      g_intervalId = setInterval("countDown()", 1000);
+      g_intervalId = setInterval("countDownWork()", 1000);
+   });
+
+   $("button.rest").click(function(e){
+      if( g_isTimerOn ) return;
       
-      //var pom = makePom();
-      //pom.insertBefore($(this));
+      var thisTask = $(this).closest(".donetask");
+      thisTask.addClass("activetask").removeClass("donetask");
+      
+      g_taskTimeElement = thisTask.find(".timer").last();
+      g_secondsLeft = 5;//25*60;
+      g_isTimerOn = true;
+      g_intervalId = setInterval("countDownRest()", 1000);
+      
    });
 
    $("button.del").click(function(e){
@@ -32,20 +50,37 @@ jQuery(document).ready(function(){
          newTask.insertAfter(thisTask);
          newInput.focus();
       }
+      clearTimeout( g_timeoutId );
+      g_timeoutId = setTimeout("saveTasksAndPomodoros()", 1000);
    });
 });
 
-function makePom() {
-   return $('<img src="css/pom.jpg" />');
+function saveTasksAndPomodoros() {
+   if(typeof(localStorage)=='undefined') { return; }
+   
+   var tasks = [];
+   $(".task input").each( function(index) {
+      tasks.push($(this).attr("value"));
+      });
+   
+   var dataJSON = $.toJSON( tasks );
+   
+   try {
+      localStorage.setItem("tasks", dataJSON);
+      g_storageErrorHappened = false;
+   } catch(e) {
+      if (e==QUOTA_EXCEEDED_ERR && !g_storageErrorHappened ) {
+         alert('Cannot save tasks, browser says the local storage space quota is exceeded. Did you create too many tasks?');
+         g_storageErrorHappened = true;
+      }
+   }
 }
 
-function grayOutAllTasksButThis( task ) {
-   //var article = task.parent("article");
-   //article.children(".task").addClass("gray");
-   //task.removeClass("gray");
+function makeDonePom() {
+   return $('<img class="pom" src="css/pom50rd.png" />');
 }
 
-function countDown() {
+function countDownWork() {
    if( --g_secondsLeft > 0 ) {
       var min = Math.floor( g_secondsLeft / 60 );
       var sec = g_secondsLeft % 60;
@@ -57,6 +92,31 @@ function countDown() {
       g_taskTimeElement.text( '00:00' );
       clearInterval( g_intervalId );
       g_isTimerOn = false;
+      var thisTask = g_taskTimeElement.closest(".activetask");
+      thisTask.addClass("donetask").removeClass("activetask");
+   }
+}
+
+function countDownRest() {
+   if( --g_secondsLeft > 0 ) {
+      var min = Math.floor( g_secondsLeft / 60 );
+      var sec = g_secondsLeft % 60;
+      var minStr = toTwoDigitString( min );
+      var secStr = toTwoDigitString( sec );
+      g_taskTimeElement.text( minStr + ':' + secStr );
+   }
+   else {
+      g_taskTimeElement.text( '00:00' );
+      clearInterval( g_intervalId );
+      g_isTimerOn = false;
+      var thisTask = g_taskTimeElement.closest(".activetask");
+      thisTask.addClass("task").removeClass("activetask");
+      makeDonePom().insertBefore(g_taskTimeElement.closest(".controls"));
+      thisTask.parent("article").find(".inactivetask").each( function(index) {
+         if ($(this) != thisTask) {
+            $(this).addClass("task").removeClass("inactivetask");
+         }
+      });
    }
 }
 
