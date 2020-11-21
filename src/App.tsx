6184fc3063c960ba11d3181cdc22516eeb0a26bd
alexandroid <b.trafficlight@gmail.com>
2020-11-20 21:58:27 -0700

    Add task type and skeleton list UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type Task = {
+  objective: string;
+  numDone: number;
+  inProgressUntilTime: number | undefined; // undefined means the task is not in progress
+  numRemaining: number;
+};
+
+type State = 'planning' | 'executing';
+
+const BLANK_TASK: Task = {
+  objective: 'Test task',
+  numDone: 0,
+  inProgressUntilTime: undefined,
+  numRemaining: 0,
+};
+
 function App() {
+  const [state, setState] = useState('planning' as State);
+  const [tasks, setTasks] = useState([BLANK_TASK]);
+
+  if (state === 'planning') {
+    return (
+      <>
+        <h1>Effort plan</h1>
+        <ul>
+          {tasks.map(task => (
+            <li>{task.objective}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+
+  // state === 'executing' at this point
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Today's goals</h1>
+      <ul>
+        {tasks.map(task => (
+          <li>{task.objective}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
