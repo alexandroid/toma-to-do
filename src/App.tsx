50a3aa8f42bd66cbed9223f07f3cdc707e350173
alexandroid <b.trafficlight@gmail.com>
2020-12-26 23:29:51 -0700

    Implement task list add/modify
    Properly rendering list of tasks, including a placeholder for the
    number of tomatoes. "Enter" adds a new task right below the
    current one and changes focus to it. "Up" and "Down" arrows move
    the focus around.
    
    Added immer library dependency (pinned to the exact version).

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
+import { PlannedTomato } from './tomato-icons';
+import produce from 'immer';
 
 type Task = {
   objective: string;
@@ -25,17 +27,45 @@ const BLANK_TASK: Task = {
 
 function App() {
   const [state, setState] = useState('planning' as State);
-  const [tasks, setTasks] = useState([BLANK_TASK]);
+  const [tasks, setTasks] = useState([produce(BLANK_TASK, draft => draft)]);
+  const [taskIndexToFocusNext, setTaskIndexToFocusNext] = useState(0);
 
   if (state === 'planning') {
     return (
       <>
         <Container>
           <h1>Effort plan</h1>
-          <Box>
-            <TextField id="one" value={tasks[0].objective} fullWidth={true} onChange={(e) => setTasks([{...tasks[0], objective: e.target.value}])}/>
-          </Box>
-          <TextField id="two" value={tasks[0].objective} />
+          {tasks.map((task, taskIndex) => {
+            return (<Box key={`task-${taskIndex}`}>
+              <TextField id={`task-${taskIndex}`} value={task.objective} /*fullWidth={true}*/
+              // https://stackoverflow.com/a/56066985/49678:
+              inputRef={input => input && taskIndex === taskIndexToFocusNext && input.focus()}
+              onChange={(e) => setTasks(produce(tasks, draft => {
+                draft[taskIndex].objective = e.target.value;
+              }))}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  console.log(`enter pressed, lastInsertedIndex: ${taskIndex+1}`);
+                  setTasks(produce(tasks, draft => {
+                    draft.splice(taskIndex+1, 0, BLANK_TASK);
+                  }));
+                  setTaskIndexToFocusNext(taskIndex+1);
+                  e.preventDefault();
+                } else if (e.key === 'ArrowUp') {
+                  if(taskIndexToFocusNext > 0) {
+                    setTaskIndexToFocusNext(taskIndexToFocusNext - 1);
+                  }
+                  e.preventDefault();
+                } else if (e.key === 'ArrowDown') {
+                  if(tasks.length > 0 && taskIndexToFocusNext < tasks.length - 1) {
+                    setTaskIndexToFocusNext(taskIndexToFocusNext + 1);
+                  }
+                  e.preventDefault();
+                }
+              }}/>
+              <PlannedTomato/>
+            </Box>);
+          })}
           {/* <Button variant="contained" color="primary">
             Hello World
           </Button> */}
