afb1e744e5c6fa4cedbfc349f82dc5f6b1a0dba9
alexandroid <b.trafficlight@gmail.com>
2020-12-27 23:57:54 -0700

    Add task deletion, tomato planning controls
    Added support of "Ctrl+Delete" for deleting tasks and "Ctrl+Up/Down"
    for increasing/decreasing planned tomato amounts.
    
    Refactored code to extract planned task view into 2 components - one
    pure renderer (view) and another handling the events (controller).

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const BLANK_TASK: Task = {
   objective: 'Test task',
   numDone: 0,
   inProgressUntilTime: undefined,
-  numRemaining: 0,
+  numRemaining: 1,
 };
 
 function App() {
@@ -35,37 +35,12 @@ function App() {
       <>
         <Container>
           <h1>Effort plan</h1>
-          {tasks.map((task, taskIndex) => {
-            return (<Box key={`task-${taskIndex}`}>
-              <TextField id={`task-${taskIndex}`} value={task.objective} /*fullWidth={true}*/
-              // https://stackoverflow.com/a/56066985/49678:
-              inputRef={input => input && taskIndex === taskIndexToFocusNext && input.focus()}
-              onChange={(e) => setTasks(produce(tasks, draft => {
-                draft[taskIndex].objective = e.target.value;
-              }))}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  console.log(`enter pressed, lastInsertedIndex: ${taskIndex+1}`);
-                  setTasks(produce(tasks, draft => {
-                    draft.splice(taskIndex+1, 0, BLANK_TASK);
-                  }));
-                  setTaskIndexToFocusNext(taskIndex+1);
-                  e.preventDefault();
-                } else if (e.key === 'ArrowUp') {
-                  if(taskIndexToFocusNext > 0) {
-                    setTaskIndexToFocusNext(taskIndexToFocusNext - 1);
-                  }
-                  e.preventDefault();
-                } else if (e.key === 'ArrowDown') {
-                  if(tasks.length > 0 && taskIndexToFocusNext < tasks.length - 1) {
-                    setTaskIndexToFocusNext(taskIndexToFocusNext + 1);
-                  }
-                  e.preventDefault();
-                }
-              }}/>
-              <PlannedTomato/>
-            </Box>);
-          })}
+          <TaskPlannerController
+            tasks={tasks}
+            setTasks={setTasks}
+            taskIndexToFocusNext={taskIndexToFocusNext}
+            setTaskIndexToFocusNext={setTaskIndexToFocusNext}
+          />
           {/* <Button variant="contained" color="primary">
             Hello World
           </Button> */}
@@ -83,6 +58,114 @@ function App() {
           <li>{task.objective}</li>
         ))}
       </ul>
+    </>
+  );
+}
+
+type TaskPlannerControllerProps = {
+  setTasks: (tasks: Task[]) => void;
+  setTaskIndexToFocusNext: (taskIndexToFocusNext: number) => void;
+  tasks: Task[];
+  taskIndexToFocusNext: number;
+}
+
+function TaskPlannerController({
+  tasks,
+  setTasks,
+  taskIndexToFocusNext,
+  setTaskIndexToFocusNext
+}: TaskPlannerControllerProps) {
+  return (
+    <TaskPlannerView
+      tasks={tasks}
+      taskIndexToFocus={taskIndexToFocusNext}
+      onChange={(event, taskIndex) => {
+        setTasks(
+          produce(tasks, draft => {
+            draft[taskIndex].objective = event.target.value;
+          })
+        )
+      }}
+      onKeyDown={(event, taskIndex) => {
+        // TODO: Hanlde 'Tab' focus change
+        if (event.key === 'Enter') {
+          //console.log(`enter pressed, lastInsertedIndex: ${taskIndex+1}`);
+          setTasks(
+            produce(tasks, draft => {
+              draft.splice(taskIndex + 1, 0, BLANK_TASK);
+            })
+          );
+          setTaskIndexToFocusNext(taskIndex + 1);
+          event.preventDefault();
+        } else if (event.key === 'ArrowUp') {
+          if (event.ctrlKey) {
+            setTasks(
+              produce(tasks, draft => {
+                draft[taskIndex].numRemaining++;
+              })
+            )
+          } else if (taskIndexToFocusNext > 0) {
+            setTaskIndexToFocusNext(taskIndexToFocusNext - 1);
+          }
+          event.preventDefault();
+        } else if (event.key === 'ArrowDown') {
+          if (event.ctrlKey) {
+            if (tasks[taskIndex].numRemaining > 1) {
+              setTasks(
+                produce(tasks, draft => {
+                  draft[taskIndex].numRemaining--;
+                })
+              )
+            }
+          } else if (tasks.length > 0 && taskIndexToFocusNext < tasks.length - 1) {
+            setTaskIndexToFocusNext(taskIndexToFocusNext + 1);
+          }
+          event.preventDefault();
+        } else if (event.key === 'Delete' && event.ctrlKey) {
+          if (tasks.length > 1) {
+            setTasks(
+              produce(tasks, draft => {
+                draft.splice(taskIndex, 1)
+              })
+            );
+            if (taskIndexToFocusNext === tasks.length - 1) {
+              setTaskIndexToFocusNext(taskIndexToFocusNext - 1);
+            }
+          }
+          event.preventDefault();
+        }
+      }}
+    />);
+}
+
+type TaskPlannerViewProps = {
+  tasks: Task[];
+  taskIndexToFocus: number;
+  onChange: (event: any, taskIndex: number) => void;
+  onKeyDown: (event: any, taskIndex: number) => void;
+};
+
+function TaskPlannerView({ tasks, taskIndexToFocus, onChange, onKeyDown }: TaskPlannerViewProps) {
+  return (
+    <>
+      {tasks.map((task, taskIndex) => {
+        return (
+          <Box key={`task-${taskIndex}`}>
+            <TextField id={`task-${taskIndex}`} value={task.objective} /*fullWidth={true}*/
+              // https://stackoverflow.com/a/56066985/49678:
+              inputRef={input => input && taskIndex === taskIndexToFocus && input.focus()}
+              onChange={(e) => onChange(e, taskIndex)}
+              onKeyDown={(e) => onKeyDown(e, taskIndex)} />
+            {new Array(task.numRemaining)
+              .fill(undefined)
+              .map((e, plannedTomatoIndex) => {
+                const key = `planned-tomato-${taskIndex}-${plannedTomatoIndex}`;
+                return <PlannedTomato key={key} />;
+              })
+            }
+          </Box>
+        );
+      })}
     </>
   );
 }
