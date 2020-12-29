29e9b946c1e83e234095939cc4214e788dd8e989
alexandroid <b.trafficlight@gmail.com>
2020-12-28 22:34:48 -0700

    Fix comment in JSX in tomato-icons
    Fixing a compiler warning that comments in JSX need to be in  {}, but
    I've just moved it out of the method.

--- src/tomato-icons.tsx
+++ src/tomato-icons.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import SvgIcon from "@material-ui/core/SvgIcon";
 
+// tomato by Saepul Nahwan from the Noun Project
+// https://thenounproject.com/saepulnahwan23/collection/fruit-and-vegetable-solid/?i=2550102
 export function PlannedTomato() {
   return (
     <SvgIcon viewBox="0 0 32 40" fontSize="large" htmlColor="#C0C0C0">
-      // tomato by Saepul Nahwan from the Noun Project
-      // https://thenounproject.com/saepulnahwan23/collection/fruit-and-vegetable-solid/?i=2550102
       <path d="M19.54,7l2-1.18a1,1,0,1,0-1-1.71L17,6.23V3a1,1,0,0,0-2,0V6.23L11.51,4.14a1,1,0,0,0-1,1.71l2,1.18A10.14,10.14,0,0,0,3.13,17.13a12.87,12.87,0,0,0,25.74,0A10.14,10.14,0,0,0,19.54,7Zm-7.3,6.09a4.14,4.14,0,0,0-3,3.09,1,1,0,0,1-1,.78L8,17a1,1,0,0,1-.76-1.19,6.08,6.08,0,0,1,4.47-4.59,1,1,0,0,1,.49,1.94Z" />
     </SvgIcon>
   )
