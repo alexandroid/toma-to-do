32ffd83ae685da5c7d130bd3bc7f01d95c8e71e0
alexandroid <b.trafficlight@gmail.com>
2020-12-31 19:45:40 -0700

    Clone task planner to task worker, add "to go"
    Copied TaskPlannerXXX widgets to TaskWorkerXXX and wired it up on the
    "/working" page.
    
    Showing "X hours Y minutes to go" on the planning page, based on the
    number of remaining tomatoes and work/rest durations. The logic of presentation is similar to the old version.

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,9 @@ import { Route, Switch, Link as RouteLink } from 'react-router-dom';
 import TaskPlannerController from './task-planner';
 import { BLANK_TASK, DEFAULT_REST_DURATION_MINS, DEFAULT_WORK_DURATION_MINS } from './data-model';
 import { PropTypes, TextField } from '@material-ui/core';
+import TaskWokerController from './task-worker';
 
-type State = 'planning' | 'working';
+// type State = 'planning' | 'working';
 
 function SettingsButton() {
   return (
@@ -30,82 +31,97 @@ function PlanTasksButton({ color }: { color: PropTypes.Color; }) {
 }
 
 function App() {
-  const [state, setState] = useState('planning' as State);
+  // const [state, setState] = useState('planning' as State);
   const [tasks, setTasks] = useState([produce(BLANK_TASK, draft => draft)]);
   const [taskIndexToFocusNext, setTaskIndexToFocusNext] = useState(0);
   const [tomatoWorkDurationMinutes, setTomatoWorkDurationMinutes] = useState(25);
   const [tomatoRestDurationMinutes, setTomatoRestDurationMinutes] = useState(5);
 
-  if (state === 'planning') {
-    return (
-      <Switch>
-        <Route path="/settings" exact={true}>
-          <Container>
-            <h1>Settings</h1>
-            <TextField
-              variant="filled"
-              type="number"
-              value={tomatoWorkDurationMinutes}
-              onChange={(e) => setTomatoWorkDurationMinutes(parseInt(e.target.value))}
-              label="Work duration, minutes"
-            /><br />&nbsp;<br />
-            <TextField
-              variant="filled"
-              type="number"
-              value={tomatoRestDurationMinutes}
-              onChange={(e) => setTomatoRestDurationMinutes(parseInt(e.target.value))}
-              label="Rest duration, minutes"
-            /><br />&nbsp;<br />
-            <Button
-              variant="contained"
-              color="secondary"
-              disabled={tomatoWorkDurationMinutes === DEFAULT_WORK_DURATION_MINS && tomatoRestDurationMinutes === DEFAULT_REST_DURATION_MINS}
-              onClick={() => {
-                setTomatoWorkDurationMinutes(DEFAULT_WORK_DURATION_MINS);
-                setTomatoRestDurationMinutes(DEFAULT_REST_DURATION_MINS);
-              }}>
-              Reset to 25/5
+  const remainingWorkDurationTotalMinutes = tasks
+    .map((task) => task.numRemaining)
+    .reduce((acc, numRemaining) => acc + numRemaining) * (tomatoWorkDurationMinutes + tomatoRestDurationMinutes);
+  const remainingWorkDurationHours = Math.floor(remainingWorkDurationTotalMinutes / 60);
+  const remainingWorkDurationMins = remainingWorkDurationTotalMinutes - remainingWorkDurationHours * 60;
+  const toGoString = computeToGoString(remainingWorkDurationHours, remainingWorkDurationMins);
+
+  return (
+    <Switch>
+      <Route path="/settings" exact={true}>
+        <Container>
+          <h1>Settings</h1>
+          <TextField
+            variant="filled"
+            type="number"
+            value={tomatoWorkDurationMinutes}
+            onChange={(e) => setTomatoWorkDurationMinutes(parseInt(e.target.value))}
+            label="Work duration, minutes"
+          /><br />&nbsp;<br />
+          <TextField
+            variant="filled"
+            type="number"
+            value={tomatoRestDurationMinutes}
+            onChange={(e) => setTomatoRestDurationMinutes(parseInt(e.target.value))}
+            label="Rest duration, minutes"
+          /><br />&nbsp;<br />
+          <Button
+            variant="contained"
+            color="secondary"
+            disabled={tomatoWorkDurationMinutes === DEFAULT_WORK_DURATION_MINS && tomatoRestDurationMinutes === DEFAULT_REST_DURATION_MINS}
+            onClick={() => {
+              setTomatoWorkDurationMinutes(DEFAULT_WORK_DURATION_MINS);
+              setTomatoRestDurationMinutes(DEFAULT_REST_DURATION_MINS);
+            }}>
+            Reset to 25/5
             </Button>&nbsp;
             <PlanTasksButton color="primary" />
-          </Container>
-        </Route>
-        <Route path="/" exact={true}>
-          <Container>
-            <h1>Planning tasks</h1>
-            <TaskPlannerController
-              tasks={tasks}
-              setTasks={setTasks}
-              taskIndexToFocusNext={taskIndexToFocusNext}
-              setTaskIndexToFocusNext={setTaskIndexToFocusNext}
-            /><br />
-            <SettingsButton />&nbsp;
+        </Container>
+      </Route>
+      <Route path="/" exact={true}>
+        <Container>
+          <h1>Planning tasks ({toGoString})</h1>
+          <TaskPlannerController
+            tasks={tasks}
+            setTasks={setTasks}
+            taskIndexToFocusNext={taskIndexToFocusNext}
+            setTaskIndexToFocusNext={setTaskIndexToFocusNext}
+          /><br />
+          <SettingsButton />&nbsp;
             <Button variant="contained" color="primary" component={RouteLink} to={"/working"}>
-              Start working
+            Start working
             </Button>
-          </Container>
-        </Route>
-        <Route path="/working">
-          <Container>
-            <h1>Working on tasks</h1>
-            <SettingsButton />&nbsp;
+        </Container>
+      </Route>
+      <Route path="/working">
+        <Container>
+          <h1>Working on tasks</h1>
+          <TaskWokerController
+            tasks={tasks}
+            setTasks={setTasks}
+            taskIndexToFocusNext={taskIndexToFocusNext}
+            setTaskIndexToFocusNext={setTaskIndexToFocusNext}
+          />
+          <SettingsButton />&nbsp;
             <PlanTasksButton color="default" />
-          </Container>
-        </Route>
-      </Switch>
-    );
-  }
-
-  // state === 'working' at this point
-  return (
-    <>
-      <h1>Today's goals</h1>
-      <ul>
-        {tasks.map(task => (
-          <li>{task.objective}</li>
-        ))}
-      </ul>
-    </>
+        </Container>
+      </Route>
+    </Switch>
   );
+}
+
+function computeToGoString(remainingWorkDurationHours: number, remainingWorkDurationMins: number) {
+  if (remainingWorkDurationHours === 0) {
+    return `${remainingWorkDurationMins} minutes to go`;
+  }
+  // remainingWorkDurationHours > 0:
+  if (remainingWorkDurationMins % 15 === 0) {
+    // If minutes are at the quarter of the hour, just show the fractional hours for brevity:
+    const fractionalHours = remainingWorkDurationHours + remainingWorkDurationMins / 60;
+    const hoursOrHour = fractionalHours === 1 ? 'hour' : 'hours'
+    return `${fractionalHours} ${hoursOrHour} to go`;
+  }
+  const hoursOrHour = remainingWorkDurationHours === 1 ? 'hour' : 'hours'
+  // Note: minutes are never zero at this point to we will not say "0 minutes to go":
+  return `${remainingWorkDurationHours} ${hoursOrHour} and ${remainingWorkDurationMins} minutes to go`;
 }
 
 export default App;
---
+++ src/task-worker/index.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import produce from 'immer';
+
+import { PlannedTomato } from '../tomato-icons';
+import { BLANK_TASK, Task } from '../data-model';
+
+export type TaskWorkerControllerProps = {
+  setTasks: (tasks: Task[]) => void;
+  setTaskIndexToFocusNext: (taskIndexToFocusNext: number) => void;
+  tasks: Task[];
+  taskIndexToFocusNext: number;
+}
+
+export default function TaskWokerController({
+  tasks,
+  setTasks,
+  taskIndexToFocusNext,
+  setTaskIndexToFocusNext
+}: TaskWorkerControllerProps) {
+  return (
+    <TaskWorkerView
+      tasks={tasks}
+      taskIndexToFocus={taskIndexToFocusNext}
+      onChange={(event, taskIndex) => {
+        setTasks(
+          produce(tasks, draft => {
+            draft[taskIndex].objective = event.target.value;
+          })
+        )
+      }}
+      onKeyDown={(event, taskIndex) => {
+        // TODO: Hanlde 'Tab' focus change
+        if (event.key === 'Enter') {
+          //console.log(`enter pressed, lastInsertedIndex: ${taskIndex+1}`);
+          setTasks(
+            produce(tasks, draft => {
+              draft.splice(taskIndex + 1, 0, BLANK_TASK);
+            })
+          );
+          setTaskIndexToFocusNext(taskIndex + 1);
+          event.preventDefault();
+        } else if (event.key === 'ArrowUp') {
+          if (event.ctrlKey) {
+            setTasks(
+              produce(tasks, draft => {
+                draft[taskIndex].numRemaining++;
+              })
+            )
+          } else if (taskIndexToFocusNext > 0) {
+            setTaskIndexToFocusNext(taskIndexToFocusNext - 1);
+          }
+          event.preventDefault();
+        } else if (event.key === 'ArrowDown') {
+          if (event.ctrlKey) {
+            if (tasks[taskIndex].numRemaining > 1) {
+              setTasks(
+                produce(tasks, draft => {
+                  draft[taskIndex].numRemaining--;
+                })
+              )
+            }
+          } else if (tasks.length > 0 && taskIndexToFocusNext < tasks.length - 1) {
+            setTaskIndexToFocusNext(taskIndexToFocusNext + 1);
+          }
+          event.preventDefault();
+        } else if (event.key === 'Delete' && event.ctrlKey) {
+          if (tasks.length > 1) {
+            setTasks(
+              produce(tasks, draft => {
+                draft.splice(taskIndex, 1)
+              })
+            );
+            if (taskIndexToFocusNext === tasks.length - 1) {
+              setTaskIndexToFocusNext(taskIndexToFocusNext - 1);
+            }
+          }
+          event.preventDefault();
+        }
+      }}
+    />);
+}
+
+type TaskWorkerViewProps = {
+  tasks: Task[];
+  taskIndexToFocus: number;
+  onChange: (event: any, taskIndex: number) => void;
+  onKeyDown: (event: any, taskIndex: number) => void;
+};
+
+function TaskWorkerView({ tasks, taskIndexToFocus, onChange, onKeyDown }: TaskWorkerViewProps) {
+  return (
+    <>
+      {tasks.map((task, taskIndex) => {
+        return (
+          <Box key={`task-${taskIndex}`}>
+            <TextField id={`task-${taskIndex}`} value={task.objective} /*fullWidth={true}*/
+              // https://stackoverflow.com/a/56066985/49678:
+              inputRef={input => input && taskIndex === taskIndexToFocus && input.focus()}
+              onChange={(e) => onChange(e, taskIndex)}
+              onKeyDown={(e) => onKeyDown(e, taskIndex)} />
+            {new Array(task.numRemaining)
+              .fill(undefined)
+              .map((e, plannedTomatoIndex) => {
+                const key = `planned-tomato-${taskIndex}-${plannedTomatoIndex}`;
+                return <PlannedTomato key={key} />;
+              })
+            }
+          </Box>
+        );
+      })}
+    </>
+  );
+}
